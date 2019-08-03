1de25215d7b88be4a4654180ec11e1d2ad8e6d41
miaboloix <MbMv2017>
2019-08-03 11:14:14 -0400

    added login page

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'one:home'
+LOGIN_URL = 'login'
--- mysite/one/urls.py
+++ mysite/one/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'one'
 urlpatterns = [
-	path('', user_views.create_user, name='sign_in'),
+	path('', user_views.create_user, name='home'),
 	path('signin/', user_views.create_user, name='sign_in'),
+	path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+	path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 	path('<int:user>/get_data/', views.get_data, name='data'),
 	path('<int:pk>/results/', views.get_results, name='results'),
 	path('<int:pk>/details/', views.details, name='detail'),
