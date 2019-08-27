c360951f1c1fa89e72ad59d65f5558811d726f55
Kristin Yim <k.yim33@gmail.com>
2019-08-27 12:26:02 -0400

    set up docker

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x9@boi3s9a$)zw3b4go*c^y0gt$!sorlzk01bbjkdt%gh+9qnd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
